ca712aecd1e1be7081eb4688b5185f381f6cde6e
Abhilash Mhaisne <55abhilash@openmailbox.org>
2016-04-22 18:04:09 +0530

    Added link to form.php in display.php

--- display.php
+++ display.php
@@ -4,6 +4,9 @@ $name = $_GET['name'];
 $age = $_GET['age'];
 $fav_team = $_GET['fav_team'];
 
-echo "Name = " . $name;
-echo "Age = " . $age;
-echo "Favorite team = " . $fav_team;
+echo "Name = " . $name . "<br>";
+echo "Age = " . $age . "<br>";
+echo "Favorite team = " . $fav_team . "<br>";
+?>
+
+<a href="form.php"> Submit another response </a>
